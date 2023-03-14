efcb73745cca4048df864702080c35dbfc2d970b
Lewis Ingleton <lewis.ingleton1@.com>
2023-03-14 23:45:36 +0000

    Added nav text

--- li-portfolio-app/src/App.js
+++ li-portfolio-app/src/App.js
@@ -1,8 +1,4 @@
 import Navbar from "./components/navbar/Navbar";
-import MyApp from "./components/navbar/Navbar";
-import ResponsiveAppBar from "./components/navbar/Navbar";
-
-
 
 function App() {
   return (
--- li-portfolio-app/src/components/navbar/Navbar.js
+++ li-portfolio-app/src/components/navbar/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { AppBar, Toolbar, Avatar, Typography } from '@mui/material'
 import logo from './li-pic-modified.jpg'
+import CodeIcon from '@mui/icons-material/Code';
+import ContactPageIcon from '@mui/icons-material/ContactPage';
 
 const Navbar = () => {
     return (
         <React.Fragment>
-            <AppBar>
+            <AppBar sx={{ background: '#F1D302', color: '#242325'}}>
                 <Toolbar>
-                    <Avatar src={logo} sx={{ width: 56, height: 56 }} />
+                    <Avatar src={logo} sx={{ width: 56, height: 56 }}/>
                     <Typography>Lewis Ingleton</Typography>
-
+                    <CodeIcon />
+                    <Typography>Projects</Typography>
+                    <ContactPageIcon />
+                    <Typography>Contact me</Typography>
                 </Toolbar>
 
             </AppBar>
