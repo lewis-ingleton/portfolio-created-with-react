1b0ddd1acb441a783ccfd51ba1755f99a0bc7ddc
Lewis Ingleton <lewis.ingleton1@.com>
2023-03-18 20:43:29 +0000

    new tab icons

--- li-portfolio-app/src/components/contact-list/ContactList.js
+++ li-portfolio-app/src/components/contact-list/ContactList.js
@@ -1,6 +1,7 @@
 import React  from "react";
 
 import './ContactList.css'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 function ContactList() {
     return (
@@ -8,16 +9,16 @@ function ContactList() {
          <h3>View my profiles and CV</h3>
             <ul>
                 <li>
-                    <a href="https://github.com/lewis-ingleton" target="blank">GitHub profile</a>
+                    <a href="https://github.com/lewis-ingleton" target="blank">GitHub profile </a><OpenInNewIcon fontSize="inherit"/>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/lewis-ingleton/" target="blank">LinkedIn profile</a>
+                    <a href="https://www.linkedin.com/in/lewis-ingleton/" target="blank">LinkedIn profile </a><OpenInNewIcon fontSize="inherit"/>
                 </li>
                 <li>
-                    <a href="mailto:lewis.ingleton1@gmail.com" target="blank">lewis.ingleton1@gmail.com</a>
+                    <a href="mailto:lewis.ingleton1@gmail.com" target="blank">lewis.ingleton1@gmail.com </a><OpenInNewIcon fontSize="inherit"/>
                 </li>
                 <li>
-                    <a href="#" target="blank">Download my CV</a>
+                    <a href="#" target="blank">Download my CV </a><OpenInNewIcon fontSize="inherit"/>
                 </li>
 
             </ul>
--- li-portfolio-app/src/pages/Home.js
+++ li-portfolio-app/src/pages/Home.js
@@ -4,7 +4,7 @@ import Header from "../components/header/Header";
 function Home() {
     const title = 'Lewis Ingleton'
     const subTitle = 'My front end developer portfolio'
-    const intro = `I'm in the early stages in my journey to become a front end developer. This mini site showcases the projects I've worked on and you can find my contact details on the Contact page.`
+    const intro = `I'm in the early stages of my journey to become a front end developer. This mini site showcases the projects I've worked on and you can also find my contact details on the Contact page.`
     return (
         <>
         <Header title={title} subTitle={subTitle} intro={intro}>
