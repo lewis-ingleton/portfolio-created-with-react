5f4299c9ca0f7be8e27595b85b8953b40f506442
Lewis Ingleton <lewis.ingleton1@.com>
2023-03-18 00:04:07 +0000

    finished project cards

--- li-portfolio-app/src/components/projectCard/ProjectCard.js
+++ li-portfolio-app/src/components/projectCard/ProjectCard.js
@@ -2,18 +2,21 @@ import React from "react";
 import './ProjectCard.css'
 
 function ProjectCard(props) {
-    return (
-        <div className="card">
-        <div className="img-container">
-        <img alt={props.title} src={props.image}/>
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.title} src={props.image} />
       </div>
       <div className="content">
         <h3>{props.title}</h3>
         <p>{props.description}</p>
-        <a href={props.link} target="_blank"><button>Go to GitHub repo</button></a>
+        <div className="flex-wrapper">
+          <a href={props.repoLink} target="_blank"><button>Go to GitHub repo</button></a>
+          <a href={props.liveLink} target="_blank"><button>Go to deployed page</button></a>
+        </div>
       </div>
     </div>
-    )
+  )
 }
 
 export default ProjectCard;
--- li-portfolio-app/src/pages/Projects.js
+++ li-portfolio-app/src/pages/Projects.js
@@ -7,48 +7,55 @@ import Header from "../components/header/Header";
 function Projects() {
     return (
         <>
-           <h1>Projects</h1>
+            <h1>Projects</h1>
             <div className="flex">
-            <ProjectCard
+                <ProjectCard
                     title={projects[0].title}
                     image={projects[0].image}
                     description={projects[0].description}
-                    link={projects[0].link}
+                    repoLink={projects[0].repoLink}
+                    liveLink={projects[0].liveLink}
+
                 />
                 <ProjectCard
                     title={projects[1].title}
                     image={projects[1].image}
                     description={projects[1].description}
-                    link={projects[1].link}
+                    repoLink={projects[1].repoLink}
+                    liveLink={projects[1].liveLink}
                 />
                 <ProjectCard
                     title={projects[2].title}
                     image={projects[2].image}
                     description={projects[2].description}
-                    link={projects[2].link}
+                    repoLink={projects[2].repoLink}
+                    liveLink={projects[2].liveLink}
                 />
                 <ProjectCard
                     title={projects[3].title}
                     image={projects[3].image}
                     description={projects[3].description}
-                    link={projects[3].link}
+                    repoLink={projects[3].repoLink}
+                    liveLink={projects[3].liveLink}
                 />
                 <ProjectCard
                     title={projects[4].title}
                     image={projects[4].image}
                     description={projects[4].description}
-                    link={projects[4].link}
+                    repoLink={projects[4].repoLink}
+                    liveLink={projects[4].liveLink}
                 />
                 <ProjectCard
                     title={projects[5].title}
                     image={projects[5].image}
                     description={projects[5].description}
-                    link={projects[5].link}
+                    repoLink={projects[5].repoLink}
+                    liveLink={projects[5].liveLink}
                 />
 
             </div>
-               
-            
+
+
 
 
         </>
