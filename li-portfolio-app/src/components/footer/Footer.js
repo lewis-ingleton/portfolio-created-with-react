74ab66954333ab73d132b6fcef198499193bb7df
Lewis Ingleton <lewis.ingleton1@.com>
2023-03-18 13:03:53 +0000

    footer edit

--- li-portfolio-app/src/components/footer/Footer.js
+++ li-portfolio-app/src/components/footer/Footer.js
@@ -1,10 +1,12 @@
 import './Footer.css'
+import GHicon from './github-mark-white.png'
 
 function Footer() {
     return (
         <>
             <footer>
                 <p>Lewis Ingleton 2023 | Made using React</p>
+                <a href="https://github.com/lewis-ingleton" target="_blank">My GitHub</a>
             </footer>
 
         </>
